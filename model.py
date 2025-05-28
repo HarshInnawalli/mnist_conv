150c5e39ced4d288b8012aa25cf467323b747cdb
HarshInnawalli <innawalliharsh@gmail.com>
2025-05-28 20:25:29 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,18 @@
+from tensorflow.keras import models, layers
+
+def build_model():
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(10, activation='softmax')
+    ])
+    return model
